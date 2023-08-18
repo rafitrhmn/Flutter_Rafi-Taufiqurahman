f43a431e8e3ab6fef84808184a62530eded4355b
rafitrhmn <rafitaufih10@gmail.com>
2023-08-18 16:17:10 +0700

    update

--- 03_Fundamental Dart/pratikum/tantangan.dart
+++ 03_Fundamental Dart/pratikum/tantangan.dart
@@ -5,8 +5,9 @@ int panjangdanlebar = panjang + lebar;
 int sisiA = 2;
 int sisiB = 5;
 int sisiC = 4;
+int sissegitiga = sisiA + sisiB + sisiC;
 
 void main(List<String> args) {
   print('keliling persegi panjang adalah ${2 * panjangdanlebar}');
-  print('keliling segitiga adalah ${sisiA + sisiB + sisiC}');
+  print('keliling segitiga adalah ${sissegitiga}');
 }
