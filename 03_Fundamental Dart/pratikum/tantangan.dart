25ad18ad28f9fde5f2bb322ccbdbd79ed42af84c
rafitrhmn <rafitaufih10@gmail.com>
2023-08-18 16:13:10 +0700

    tantangan live

---
+++ 03_Fundamental Dart/pratikum/tantangan.dart
@@ -0,0 +1,12 @@
+//keliling persegi panjang dan keliling segitiga
+int panjang = 5;
+int lebar = 4;
+int panjangdanlebar = panjang + lebar;
+int sisiA = 2;
+int sisiB = 5;
+int sisiC = 4;
+
+void main(List<String> args) {
+  print('keliling persegi panjang adalah ${2 * panjangdanlebar}');
+  print('keliling segitiga adalah ${sisiA + sisiB + sisiC}');
+}
