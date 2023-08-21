5020fd8b9ace79f07663afcb7a986f0c3d8b5ab6
rafitrhmn <rafitaufih10@gmail.com>
2023-08-21 10:47:49 +0700

    create soal prioritas 1 part 2
    soal prioritas 1 part 2. keliling dan luas lingkaran

---
+++ 03_Fundamental Dart/pratikum/soal-prioritas-1_part-2.dart
@@ -0,0 +1,22 @@
+///soal prioritas 1 nomor 2
+///
+//nama : Rafi Taufiqurahman
+///program : Flutter kelas D (Basic)
+///
+///membuat program luas dan keliling lingkaran
+///
+double luasLingkaran(double nilaiPi, int jarijari) =>
+    nilaiPi * jarijari * jarijari;
+
+double kelilinglingkaran(double nilaiPi, int jarijari) =>
+    nilaiPi * 2 * jarijari;
+void main(List<String> args) {
+  double nilaiPi = 3.14;
+  int jarijari = 6;
+  //
+  var a = luasLingkaran(nilaiPi, jarijari);
+  print('luas lingkaran dengan jari jari $jarijari cm adalah = $a cm');
+  //
+  var b = kelilinglingkaran(nilaiPi, jarijari);
+  print('keliling lingkaran dengan jari jari $jarijari cm adalah = $b cm');
+}
