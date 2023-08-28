bbc6cf9509ef8a876999f23588d6ecbf5a4215e0
rafitrhmn <rafitaufih10@gmail.com>
2023-08-28 10:06:24 +0700

    create tugas function
    membuat tugas function dan screenshootnya

---
+++ 04_brancing-looping-function/pratikum/soal-prioritas-2_tugas-function.dart
@@ -0,0 +1,19 @@
+///soal prioritas 1
+///
+//nama : Rafi Taufiqurahman
+///program : Flutter kelas D (Basic)
+///
+// Buatlah sebuah function dengan parameter untuk menghitung luas lingkaran.
+// kemudian function lingkaran tersebut dijalankan pada function main!
+
+double luasLingakaran({int? jariJari}) {
+  var nilaiPi = 3.14;
+  return nilaiPi * jariJari! * jariJari;
+}
+
+void main(List<String> args) {
+  var nilaiJariJari = 6;
+  var luasLingkaran2 = luasLingakaran(jariJari: nilaiJariJari);
+  print(
+      'luas lingkaran dengan jari-jari $nilaiJariJari adalah $luasLingkaran2');
+}
